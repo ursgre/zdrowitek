5d4edbcfbf41f24d42c2ebca2d2153c68b51463b
ursgre <urszulagrezicka@gmail.com>
2023-12-17 13:47:41 +0100

    sidebar links

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,16 @@
+import { sidebarLinks } from "@/constants";
+
 function LeftSidebar() {
-    return <h1>LeftSidebarbar</h1>
+    return (
+        <section className='custom-scrollbar leftsidebar'>
+             <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+             {sidebarLinks.map((link) => (
+                <div>Link</div>
+             )) }
+             </div>
+
+        </section>
+    )
 }
 
 export default LeftSidebar;
