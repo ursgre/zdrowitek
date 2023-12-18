0f1fa1ed47beec5fa863916d28663e831f07b9e7
ursgre <urszulagrezicka@gmail.com>
2023-12-18 19:35:23 +0100

    ask question

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -47,9 +47,37 @@ function PostThread({ userId }: {userId: string}) {
       }
       
     })
-  
     
-    return <h1>Post thread form</h1>
+    const onSubmit = () => {
+    }
+    
+    return (
+    <Form {...form}>
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+                    control={form.control}
+                    name='thread'
+                    render={({ field }) => (
+                      <FormItem className='flex w-full flex-col gap-3'>
+                        <FormLabel className='text-base-semibold text-light-2'>
+                          Question
+                        </FormLabel>
+                        <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                          <Textarea
+                            rows={15}
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+      </form>
+    </Form>
+    )
 }
 
 export default PostThread;
