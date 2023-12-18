110e09cac214c82be85246c472e71df759642ae6
ursgre <urszulagrezicka@gmail.com>
2023-12-18 17:36:47 +0100

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,8 +2,27 @@
 import { connectToDB } from "../mongoose";
 import User from "../models/user.model";
 
-export async function updateUser(userId: string): Promise<void> {
+export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+    ): Promise<void> {
     connectToDB();
 
-    await User.findOneAndUpdate({ id: userId } )
+    await User.findOneAndUpdate(
+        { id: userId },
+        { 
+            username: username.toLowerCase(),
+            name,
+            bio,
+            image,
+            onboarded: true,
+        },
+        { upsert: true }
+        );
+
+        if(path ===)
 }
