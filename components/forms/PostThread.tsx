d923958eee26a3759b6220535f148375124315b5
ursgre <urszulagrezicka@gmail.com>
2023-12-18 19:06:45 +0100

    posts

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,10 @@
+import { usePathname, useRouter } from "next/navigation";
+
+
+function PostThread({ userId }: {userId: string}) {
+    
+    return <h1>Post thread form</h1>
+}
+
+export default PostThread;
+  
