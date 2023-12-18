953645446b970e7b9a101dab5107da4d72c9b1d6
ursgre <urszulagrezicka@gmail.com>
2023-12-18 16:36:28 +0100

    user profile image change

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,7 +20,8 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+
 
 interface Props {
     user: {
@@ -37,13 +38,14 @@ interface Props {
 
 const AccountProfile = ({user, btnTitle}: Props) => {
 
+  const [files, setFiles] = useState<File[]>([]);
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo:  '',
-      name:  '',
-      username:  '',
-      bio:  '',
+      profile_photo:  user?.image || "",
+      name:   user?.name || "",
+      username:   user?.username || "",
+      bio:   user?.bio || "",
     },
   })
 
@@ -52,8 +54,25 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     console.log(values)
   }
 
-  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
-    e.preventDefault(); }
+  const handleImage = ( e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void) => {
+    e.preventDefault(); 
+  
+    const fileReader = new FileReader();
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+        fieldChange(imageDataUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
+  }
 
     return(
       <Form {...form}>
