084698db2b6875940259389c5a91e17840e277ef
ursgre <urszulagrezicka@gmail.com>
2023-12-18 23:11:14 +0100

    Update Comment.tsx

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,4 +1,11 @@
-const Comment = () => {
+interface Props {
+    threadId: string;
+    currentUserImg: string;
+    currentUserId: string;
+  }
+  
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
     return(
         <div>
             <h1 className="text-white">Comment</h1>
