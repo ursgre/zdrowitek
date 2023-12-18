5d44ae5e8bbd7782241c549ecdc11c30bf9ba9a8
ursgre <urszulagrezicka@gmail.com>
2023-12-18 18:40:42 +0100

    ask questions

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,4 +1,10 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 async function Page() {
+    const user = await currentUser();
+    if (!user) return null;
+    const userInfo = await fetchUser(user.id);
     return <h1 className="head-text">Ask questions</h1>
 }
 
