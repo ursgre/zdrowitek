a09e7a74d92349fe6dad0a2ee25feac282401ce9
ursgre <urszulagrezicka@gmail.com>
2023-12-18 22:57:16 +0100

    Update thread.actions.ts

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -67,6 +67,39 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   return { posts, isNext };
 }
 
-export async function fetchThreadById(threadId: string) {
+export async function fetchThreadById(id: string) {
   connectToDB();
+
+  try{
+    const thread = await Thread.findById(id)
+    .populate({
+      path: "author",
+      model: User,
+      select: "_id id name image",
+    })
+    .populate({
+      path: "children", 
+      populate: [
+        {
+          path: "author", 
+          model: User,
+          select: "_id id name parentId image", 
+        },
+        {
+          path: "children", 
+          model: Thread, 
+          populate: {
+            path: "author", 
+            model: User,
+            select: "_id id name parentId image", 
+          }
+        }
+      ]
+    }).exec();
+
+    return thread;
+  } catch (error: any){
+    throw new Error('Error while fetching comments: ${error.message}');
+  }
+
 }
