8116dc01f16e24415cf56cce0ba6a3f915b90ca4
ursgre <urszulagrezicka@gmail.com>
2023-12-18 20:32:25 +0100

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
- export default function Home() {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+// import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+ 
+ export default async function Home() {
+
+  // const result = await fetchPosts();
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
