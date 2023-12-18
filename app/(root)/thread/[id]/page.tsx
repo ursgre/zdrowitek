a0f52d585e8d204dc73837a33f0119a55d36057f
ursgre <urszulagrezicka@gmail.com>
2023-12-18 22:44:46 +0100

    Update page.tsx

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 
 
 
+
 const Page = async ({ params } : { params: { id: string }}) => {
     if(!params.id) return null;
     const user = await currentUser();
@@ -13,20 +14,22 @@ const Page = async ({ params } : { params: { id: string }}) => {
     const userInfo = await fetchUser(user.id);
     if (!userInfo?.onboarded) redirect("/onboarding");
 
+    const thread = await fetchThreadById(params.id);
+
 
     return(
     <section className="relative">
         <div>
         <ThreadCard
-            key={post._id}
-            id={post._id}
+            key={thread._id}
+            id={thread._id}
             currentUserId={user?.id || ""}
-            parentId={post.parentId}
-            content={post.text}
-            author={post.author}
-            community={post.community}
-            createdAt={post.createdAt}
-            comments={post.children}
+            parentId={thread.parentId}
+            content={thread.text}
+            author={thread.author}
+            community={thread.community}
+            createdAt={thread.createdAt}
+            comments={thread.children}
               />
         </div>
     </section>
