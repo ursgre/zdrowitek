120cb0d56e6814d68beb675fd62136b92ee50ce3
ursgre <urszulagrezicka@gmail.com>
2023-12-18 23:19:25 +0100

    comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,3 +1,20 @@
+"use client"
+import { useForm } from "react-hook-form";
+import { usePathname } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { z } from "zod";
+import { CommentValidation } from "@/lib/validations/thread";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+  } from "@/components/ui/form";
+
 interface Props {
     threadId: string;
     currentUserImg: string;
@@ -6,10 +23,58 @@ interface Props {
   
 
 const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+    const pathname = usePathname();
+
+    const form = useForm<z.infer<typeof CommentValidation>>({
+        resolver: zodResolver(CommentValidation),
+        defaultValues: {
+        thread: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+        // await addCommentToThread(
+        // threadId,
+        // values.thread,
+        // JSON.parse(currentUserId),
+        // pathname
+        // );
+    }
+
     return(
-        <div>
-            <h1 className="text-white">Comment</h1>
-        </div>
+        <Form {...form}>
+        <form className='comment-form' onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name='thread'
+            render={({ field }) => (
+              <FormItem className='flex w-full items-center gap-3'>
+                <FormLabel>
+                  <Image
+                    src={currentUserImg}
+                    alt='current_user'
+                    width={48}
+                    height={48}
+                    className='rounded-full object-cover'
+                  />
+                </FormLabel>
+                <FormControl className='border-none bg-transparent'>
+                  <Input
+                    type='text'
+                    {...field}
+                    placeholder='Comment...'
+                    className='no-focus text-light-1 outline-none'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+  
+          <Button type='submit' className='comment-form_btn'>
+            Reply
+          </Button>
+        </form>
+      </Form>
     )
 }
 
