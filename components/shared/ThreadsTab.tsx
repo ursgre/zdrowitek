0989516d5c77494a652a53d509c914c7550b6b1e
ursgre <urszulagrezicka@gmail.com>
2023-12-19 01:47:10 +0100

    Update ThreadsTab.tsx

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
+import { fetchUserPosts } from "@/lib/actions/user.actions";
 
 interface Props {
     currentUserId: string;
@@ -8,11 +9,29 @@ interface Props {
   }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+    let result = await fetchUserPosts(accountId);
+
+    if (!result) {
+        redirect("/");
+      }
+
     return (
-        <section>
-            ThreadsTab
+        <section className='mt-9 flex flex-col gap-10'>
+            {result.threads.map((thread: any) => (
+            <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={currentUserId}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+                />
+            ))}
         </section>
-    )
+    );
 }
 
 export default ThreadsTab;
