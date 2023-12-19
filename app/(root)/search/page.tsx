3867bc08225e8e0bea682ddb99a76eea93fc8c50
ursgre <urszulagrezicka@gmail.com>
2023-12-19 11:32:00 +0100

    search

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,23 @@
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+
 const Page = async () => {
+    const user = await currentUser();
+    if (!user) return null;
+  
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25,
+      })
+
+
     return(
         <section>
             <h1 className="head-text mb-10">Search</h1>
