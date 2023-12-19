0e5e71b2ce7650083f14fa699053b68c1951be9d
ursgre <urszulagrezicka@gmail.com>
2023-12-19 21:18:23 +0100

    min0

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,13 +1,11 @@
-"use client"
+"use client";
 
+import * as z from "zod";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import * as z from "zod"
-import { isBase64Image } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
-
+import { ChangeEvent, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Form,
@@ -17,44 +15,44 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { UserValidation } from "@/lib/validations/user";
-import Image from "next/image";
-import { ChangeEvent, useState } from "react";
 import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
+
+import { UserValidation } from "@/lib/validations/user";
 import { updateUser } from "@/lib/actions/user.actions";
 
-
 interface Props {
-    user: {
-      id: string;
-      objectId: string;
-      username: string;
-      name: string;
-      bio: string;
-      image: string;
-    };
-    btnTitle: string;
-  }
-  
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
 
-const AccountProfile = ({user, btnTitle}: Props) => {
-
+const AccountProfile = ({ user, btnTitle }: Props) => {
   const router = useRouter();
   const pathname = usePathname();
   const { startUpload } = useUploadThing("media");
+
   const [files, setFiles] = useState<File[]>([]);
-  const form = useForm({
+
+  const form = useForm<z.infer<typeof UserValidation>>({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo:  user?.image || "",
-      name:   user?.name || "",
-      username:   user?.username || "",
-      bio:   user?.bio || "",
+      profile_photo: user?.image ? user.image : "",
+      name: user?.name ? user.name : "",
+      username: user?.username ? user.username : "",
+      bio: user?.bio ? user.bio : "",
     },
-  })
+  });
 
   const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo;
@@ -82,16 +80,16 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     } else {
       router.push("/");
     }
-  
+  };
 
-  }
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) => {
+    e.preventDefault();
 
-
-  const handleImage = ( e: ChangeEvent<HTMLInputElement>,
-    fieldChange: (value: string) => void) => {
-    e.preventDefault(); 
-  
     const fileReader = new FileReader();
+
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       setFiles(Array.from(e.target.files));
@@ -105,17 +103,20 @@ const AccountProfile = ({user, btnTitle}: Props) => {
 
       fileReader.readAsDataURL(file);
     }
-  }
+  };
 
-    return(
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
+  return (
+    <Form {...form}>
+      <form
+        className='flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
         <FormField
           control={form.control}
-          name="profile_photo"
+          name='profile_photo'
           render={({ field }) => (
-            <FormItem className="flex items-center gap-4">
-                <FormLabel className='account-form_image-label'>
+            <FormItem className='flex items-center gap-4'>
+              <FormLabel className='account-form_image-label'>
                 {field.value ? (
                   <Image
                     src={field.value}
@@ -134,13 +135,12 @@ const AccountProfile = ({user, btnTitle}: Props) => {
                     className='object-contain'
                   />
                 )}
-                </FormLabel>
-
-                <FormControl className='flex-1 text-base-semibold text-gray-100'>
+              </FormLabel>
+              <FormControl className='flex-1 text-base-semibold text-gray-200'>
                 <Input
                   type='file'
                   accept='image/*'
-                  placeholder='Add your photo'
+                  placeholder='Add profile photo'
                   className='account-form_image-input'
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
@@ -149,69 +149,72 @@ const AccountProfile = ({user, btnTitle}: Props) => {
           )}
         />
 
-          <FormField
-                    control={form.control}
-                    name='name'
-                    render={({ field }) => (
-                      <FormItem className='flex w-full flex-col gap-3'>
-                        <FormLabel className='text-base-semibold text-light-2'>
-                          Name
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            type='text'
-                            className='account-form_input no-focus'
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          <FormField
-                    control={form.control}
-                    name='username'
-                    render={({ field }) => (
-                      <FormItem className='flex w-full flex-col gap-3'>
-                        <FormLabel className='text-base-semibold text-light-2'>
-                          Username
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            type='text'
-                            className='account-form_input no-focus'
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-            <FormField
-                      control={form.control}
-                      name='bio'
-                      render={({ field }) => (
-                        <FormItem className='flex w-full flex-col gap-3'>
-                          <FormLabel className='text-base-semibold text-light-1'>
-                            Bio
-                          </FormLabel>
-                          <FormControl>
-                            <Textarea
-                              rows={10}
-                              className='account-form_input no-focus'
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />             
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name='bio'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Bio
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  rows={10}
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit' className='bg-primary-500'>
+          {btnTitle}
+        </Button>
       </form>
     </Form>
-    )
-}
+  );
+};
 
 export default AccountProfile;
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -4,14 +4,14 @@ export const UserValidation = z.object({
     profile_photo: z.string().url().nonempty(),
     name: z
       .string()
-      .min(4, { message: "Minimum 4 characters." })
+      .min(2, { message: "Minimum 2 characters." })
       .max(20, { message: "Maximum 20 caracters." }),
     username: z
       .string()
-      .min(4, { message: "Minimum 4 characters." })
+      .min(2, { message: "Minimum 2 characters." })
       .max(20, { message: "Maximum 20 caracters." }),
     bio: z
       .string()
-      .min(4, { message: "Minimum 4 characters." })
+      .min(0, { message: "Minimum 0 characters." })
       .max(1000, { message: "Maximum 1000 caracters." }),
   });
