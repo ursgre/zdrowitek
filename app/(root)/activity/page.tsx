3d1bd9ca82033879200cf9aca5d799b08c9312da
ursgre <urszulagrezicka@gmail.com>
2023-12-19 12:31:49 +0100

    activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,23 @@
-const Page = async () => {
-    return(
-        <section>
-            <h1 className="head-text mb-10">Activity</h1>
-        </section>
-    )
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser} from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return(
+
+  );
+
+
+  
 }
 
 export default Page
