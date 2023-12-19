218dd5c6e91560b01821acbe40f16834c5a1fabb
ursgre <urszulagrezicka@gmail.com>
2023-12-19 02:00:08 +0100

    prawidłowe przekierowani na profil

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -9,6 +9,7 @@ import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className='custom-scrollbar leftsidebar'>
@@ -18,6 +19,8 @@ function LeftSidebar() {
                 const isActive =
                 (pathname.includes(link.route) && link.route.length > 1) ||
                 pathname === link.route;
+
+                if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
                 return(
                     <Link
