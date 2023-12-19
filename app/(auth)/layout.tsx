cb3a08d28a2778b14df21b018db97c94a1fd8a90
ursgre <urszulagrezicka@gmail.com>
2023-12-19 12:46:21 +0100

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
       >
         <html lang='en'>
           <body className={'${inter.className} bg-dark-1'}>
-           {children}
+            <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+            </div>
+           
           </body>
         </html>
       </ClerkProvider>
