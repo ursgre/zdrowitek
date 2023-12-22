43a1dc271d9ca35eba7c297ed4bb15b57299adbb
ursgre <urszulagrezicka@gmail.com>
2023-12-22 19:17:13 +0100

    średnia snu

--- components/forms/SleepIntake.tsx
+++ components/forms/SleepIntake.tsx
@@ -1,18 +1,21 @@
 "use client";
-// Existing imports
+// Existing imports...
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { fetchLastSevenDaysSleep, addSleepIntake, deleteSleepIntake } from "@/lib/actions/sleepIntake.actions";
 
-// ... (other existing imports)
-
 function AddSleepIntake({ userId }: { userId: string }) {
   const [amount, setAmount] = useState(0);
   const [lastSevenDaysIntake, setLastSevenDaysIntake] = useState<any[]>([]); // State to store last seven days' intake
+  const [averageSleep, setAverageSleep] = useState<number | null>(null); // State to store average sleep
 
   useEffect(() => {
     fetchLastSevenDaysIntake();
   }, [userId]);
+
+  useEffect(() => {
+    calculateAverageSleep();
+  }, [lastSevenDaysIntake]);
 
   const fetchLastSevenDaysIntake = async () => {
     try {
@@ -20,29 +23,31 @@ function AddSleepIntake({ userId }: { userId: string }) {
       setLastSevenDaysIntake(intakeForLastSevenDays);
     } catch (error) {
       console.error("Error fetching sleep intake for the last seven days:", error);
-      // Handle error scenarios
+      // Error handling
     }
+  };
+
+  const calculateAverageSleep = () => {
+    const totalSleep = lastSevenDaysIntake.reduce((total, intake) => total + intake.amount, 0);
+    const average = totalSleep / lastSevenDaysIntake.length || 0;
+    setAverageSleep(average);
   };
 
   const handleAddIntake = async () => {
     try {
       if (!isNaN(amount) && amount > 0) {
-        // Fetch existing sleep intake to check if already added for the last seven days
         const lastSevenDaysIntake = await fetchLastSevenDaysSleep(userId);
 
         if (lastSevenDaysIntake.length > 0) {
-          // If sleep intake already exists for the last seven days, show an alert
           alert("Sleep intake already added for today or in the last seven days. You cannot add more than once in a day.");
         } else {
-          // If no sleep intake for the last seven days, proceed with adding intake
           await addSleepIntake({ amount, userId });
-          // Fetch and update displayed sleep intake after adding intake
           await fetchLastSevenDaysIntake();
-          setAmount(0); // Reset input after adding intake
+          setAmount(0);
         }
       } else {
         console.error("Invalid input for sleep intake");
-        // Handle invalid input scenarios (non-numeric or negative values)
+        // Handle invalid input scenarios
       }
     } catch (error) {
       console.error("Error adding sleep intake:", error);
@@ -52,8 +57,8 @@ function AddSleepIntake({ userId }: { userId: string }) {
 
   const handleDeleteIntake = async (index: number) => {
     try {
-      // Your delete sleep intake logic goes here based on the index or intake object
-      // Ensure correct handling of deletion and UI update
+      await deleteSleepIntake({ deletedAmount: lastSevenDaysIntake[index].amount, userId });
+      await fetchLastSevenDaysIntake();
     } catch (error) {
       console.error("Error deleting sleep intake:", error);
       // Handle error scenarios
@@ -66,17 +71,16 @@ function AddSleepIntake({ userId }: { userId: string }) {
         type="number"
         value={!isNaN(amount) ? amount : ''}
         onChange={(e) => setAmount(parseInt(e.target.value))}
-        placeholder="Enter amount of sleep (in hours)"
+        placeholder="Enter sleep duration (in hours)"
         className="inputField"
       />
       <button className="bg-sky-500 text-white px-4 py-2 rounded addButton" onClick={handleAddIntake}>
-        Add Sleep Intake
+        Add Sleep Duration
       </button>
-      {/* Displaying last seven days' sleep intake amounts */}
       <ul className="white-text intakeList">
         {lastSevenDaysIntake.map((intake, index) => (
           <li key={index} className="intakeListItem">
-            {`${intake.date.toDateString()}: ${intake.amount} hours`}
+            {`${new Date(intake.date).toDateString()}: ${intake.amount} hours`}
             <button
               className="bg-red-500 text-white px-2 ml-2 rounded deleteButton"
               onClick={() => handleDeleteIntake(index)}
@@ -86,9 +90,17 @@ function AddSleepIntake({ userId }: { userId: string }) {
           </li>
         ))}
       </ul>
+      <div className="averageSleep text-white">
+        {averageSleep !== null ? (
+          <p>
+            Average sleep for the last 7 days: {averageSleep.toFixed(2)} hours
+          </p>
+        ) : (
+          <p>No data available for calculating average sleep</p>
+        )}
+      </div>
     </div>
   );
 }
 
 export default AddSleepIntake;
-
